d38bb712b7447e787df50ef2e54b4e68d72f8482
leandro Rafael <leandrorafaelphoenix@gmail.com>
2021-09-03 23:59:45 -0300

    primero exercio pra relembra

--- src/pacote/ainda.java
+++ src/pacote/ainda.java
@@ -1,9 +1,21 @@
 package pacote;
 
+import java.util.Scanner;
+
 public class ainda {
 	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		System.out.println("escreva um numero");
+		 int x;
+		 int y;
+		 int v;
+		 x =ler.nextInt();
+		 System.out.println("proximo numero");
+		  y = ler.nextInt();
+		  v = x + y;
+		  System.out.println("resultado é " + v);
+		  
+		  ler.close();
 		
-		System.out.println("bom dia");
-		System.out.println("bom dia");
 	}
 }
